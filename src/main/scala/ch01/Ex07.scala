a7e6935916a97cdf9f3bf2540cfc4eaa0e7ad37d
akthrms <akt.hrms@gmail.com>
2020-06-18 02:37:26 +0900

    fix

--- src/main/scala/ch01/Ex07.scala
+++ src/main/scala/ch01/Ex07.scala
@@ -3,7 +3,7 @@ package ch01
 /**
  * https://nlp100.github.io/ja/ch01.html#07-%E3%83%86%E3%83%B3%E3%83%97%E3%83%AC%E3%83%BC%E3%83%88%E3%81%AB%E3%82%88%E3%82%8B%E6%96%87%E7%94%9F%E6%88%90
  */
-object EX07 {
+object Ex07 {
 
   def run(x: String, y: String, z: String): String = s"${x}の時の${y}は${z}"
 
