0fb06b68e496530da7aa23c806d7239b45432aa0
akthrms <akt.hrms@gmail.com>
2020-06-18 03:05:30 +0900

    update

---
+++ src/main/scala/ch01/Ex09.scala
@@ -0,0 +1,26 @@
+package ch01
+
+import scala.util.Random
+
+/**
+ * https://nlp100.github.io/ja/ch01.html#09-typoglycemia
+ */
+object Ex09 {
+
+  def run(
+    str: String = "I couldn’t believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
+  ): String = {
+    val r = """^(.)(.{3,})(.)$""".r
+    str
+      .split(" ")
+      .map {
+        word =>
+          word match {
+            case r(x, y, z) => x + Random.shuffle(y) + z
+            case _ => word
+          }
+      }
+      .mkString(" ")
+  }
+
+}
